4ad00194de87e05ebe8768c5f970917c0747c31d
Yoshi3lines <ny3line@gmail.com>
2021-11-03 01:21:16 +0900

    notificationのmodelを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_092315) do
+ActiveRecord::Schema.define(version: 2021_11_02_140356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 2021_10_26_092315) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.boolean "display_flag", default: true
+    t.bigint "staff_id"
+    t.bigint "store_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["staff_id"], name: "index_notifications_on_staff_id"
+    t.index ["store_id"], name: "index_notifications_on_store_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -133,6 +145,8 @@ ActiveRecord::Schema.define(version: 2021_10_26_092315) do
   end
 
   add_foreign_key "carts", "users"
+  add_foreign_key "notifications", "staffs"
+  add_foreign_key "notifications", "stores"
   add_foreign_key "orders", "carts"
   add_foreign_key "orders", "items"
   add_foreign_key "payments", "carts"
