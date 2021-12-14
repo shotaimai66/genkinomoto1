dca2bf119d8976dfa2d2b7618cfd54d777b9f1e4
tomooshima0828 <tomo.oshima0828@gmail.com>
2021-12-14 23:19:29 +0900

    event ticket show modified for correct counting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -60,7 +60,7 @@ class EventsController < ApplicationController
 
   private
     def event_params
-      params.require(:event).permit(:title, :category, :price, :image, :description, :location, :first_date, :last_date, :start_time, :end_time, :start_time_alt, :end_time_alt, :remaining_ticket_numbers, :status, :owner_id, :store_id)
+      params.require(:event).permit(:title, :category, :price, :stock, :image, :description, :location, :first_date, :last_date, :start_time, :end_time, :start_time_alt, :end_time_alt, :remaining_ticket_numbers, :status, :owner_id, :store_id)
     end
 
     def set_q
