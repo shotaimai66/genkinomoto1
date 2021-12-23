af14e16641212c8e55a9b8668aa3b5bace587460
tomooshima0828 <tomo.oshima0828@gmail.com>
2021-12-23 15:56:48 +0900

    heroku.yml

--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,16 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
+# 本番環境設定のため追加しました
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
+
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-#
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# 本番環境設定のため追加しました
+preload_app!
+rackup      DefaultRackup
+
+# 本番環境設定のためコメントアウトしました
+# worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -22,7 +21,13 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# 本番環境設定のためコメントアウトしました
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# 本番環境設定のため追加しました
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -40,4 +45,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# 本番環境設定のためコメントアウトしました
+# plugin :tmp_restart
